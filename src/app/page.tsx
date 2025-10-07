b172e963d7ef23b07cf15773614c273008008ac0
Vera Soniya <septiverasoniya99@gmail.com>
2025-10-07 07:10:26 +0700

    add home section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,71 @@
+'use client';
+import { techList } from '@/assets/datas/tech';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
+import Molufei from '@/assets/svg/molufei.svg';
+import Paper1 from '@/assets/svg/paper-1.svg';
+import Paper2 from '@/assets/svg/paper-2.svg';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const images = [Paper1, Paper2];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  return (
+    <div>
+      <div id="home">
+        <div className="relative w-full">
+          <motion.div
+            initial={{ opacity: 0, y: 0 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <Image src={Molufei} alt="molufei" className="w-full" />
+          </motion.div>
+          {images.map((paper, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 1.4, x: 1.4, scale: 1.4 }}
+              animate={{ opacity: 1, y: 0, x: 0, scale: 1 }}
+              transition={{ duration: 0.8, delay: index * 0.8 }}
+              className="w-full absolute top-0"
+            >
+              <Image src={paper} alt="paper" className="w-full" />
+            </motion.div>
+          ))}
+          <div className="w-full absolute top-0 flex justify-end items-center h-full px-32">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.8, delay: 1.6 }}
+              className="space-y-4"
+            >
+              <h3 className="text-5xl">Hi</h3>
+              <h3 className="text-7xl font-montserrat font-semibold">
+                It's{' '}
+                <span className="text-secondary font-montserrat">
+                  Vera Soniya
+                </span>
+              </h3>
+              <div className="flex gap-6 justify-end">
+                {techList.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col items-center justify-center gap-1"
+                  >
+                    <Image
+                      src={item.icon}
+                      alt="next-js"
+                      className="text-tertiary"
+                      width={40}
+                      height={40}
+                    />
+                    <p className="text-xs text-center">{item.label}</p>
+                  </div>
+                ))}
+              </div>
+            </motion.div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
---
+++ src/assets/datas/tech.ts
@@ -0,0 +1,27 @@
+import NextJsIcon from '@/assets/svg/next-js.svg';
+import ReactJsIcon from '@/assets/svg/react-js.svg';
+import NuxtJsIcon from '@/assets/svg/nuxt-js.svg';
+import VueJsIcon from '@/assets/svg/vue-js.svg';
+
+export const techList = [
+  {
+    label: 'Vue JS',
+    icon: VueJsIcon,
+  },
+  {
+    label: 'Nuxt JS',
+    icon: NuxtJsIcon,
+  },
+  {
+    label: 'React Native',
+    icon: ReactJsIcon,
+  },
+  {
+    label: 'React JS',
+    icon: ReactJsIcon,
+  },
+  {
+    label: 'Next JS',
+    icon: NextJsIcon,
+  },
+];
