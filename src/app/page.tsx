f94d50e91f1c50c6b29b8d2846090f4c117ab12e
Vera Soniya <septiverasoniya99@gmail.com>
2025-11-23 18:31:48 +0700

    add experience mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import Watch from '@/assets/svg/watch.svg';
-import WatchDark from '@/assets/svg/watch-dark.svg';
+
 import Paper3 from '@/assets/svg/paper-3.svg';
 import Paper4 from '@/assets/svg/paper-4.svg';
 import Paper3Dark from '@/assets/svg/paper-3-dark.svg';
@@ -30,11 +29,11 @@ import MainTooltip from '@/components/main-tooltip';
 import ContactElement from '@/components/elements/contact';
 import { useTheme } from 'next-themes';
 import ItsMeElement from '@/components/elements/itsme';
+import ExperienceElement from '@/components/elements/experience';
 
 export default function Home() {
   const { resolvedTheme } = useTheme();
 
-  const watchImage = resolvedTheme === 'light' ? Watch : WatchDark;
   const paper3Image = resolvedTheme === 'light' ? Paper3 : Paper3Dark;
   const paper4Image = resolvedTheme === 'light' ? Paper4 : Paper4Dark;
   const [mounted, setMounted] = useState(false);
@@ -61,58 +60,7 @@ export default function Home() {
       </AnimateSection>
 
       <AnimateSection id="experience">
-        <div className="relative w-full h-screen flex flex-col space-y-2 md:space-y-6 py-14 px-4 md:px-16 lg:px-32">
-          <h1 className="text-center font-montserrat text-5xl font-semibold">
-            Experiences
-          </h1>
-          <div className="flex items-center flex-1 gap-2 md:gap-4 lg:gap-6">
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.8 }}
-              className="relative w-2/5 aspect-square hidden lg:block"
-            >
-              <Image
-                src={watchImage}
-                alt="watch"
-                className="w-full h-full p-10 object-contain inset-0"
-              />
-            </motion.div>
-            <div className="flex-1">
-              <Accordion
-                type="single"
-                collapsible
-                className="w-full flex flex-col justify-start gap-4 h-full"
-                defaultValue="0"
-              >
-                {experiences.map((item, index) => (
-                  <motion.div
-                    key={index}
-                    initial={{ opacity: 0, y: -50 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8, delay: index * 0.5 }}
-                    viewport={{ once: true }}
-                    className="border-b border-tertiary/50 dark:border-tertiary/10 px-4"
-                  >
-                    <AccordionItem value={`${item.id}`}>
-                      <AccordionTrigger className="w-full cursor-pointer pb-2 hover:no-underline hover:scale-[1.02]">
-                        <ExperienceList
-                          key={index}
-                          periode={item.periode}
-                          role={item.role}
-                          office={item.office}
-                        />
-                      </AccordionTrigger>
-                      <AccordionContent className="flex flex-col gap-4 text-balance py-2 w-full">
-                        <div className="w-full text-sm">{item.description}</div>
-                      </AccordionContent>
-                    </AccordionItem>
-                  </motion.div>
-                ))}
-              </Accordion>
-            </div>
-          </div>
-        </div>
+        <ExperienceElement theme={resolvedTheme as string} />
       </AnimateSection>
 
       <AnimateSection id="projects">
---
+++ src/components/elements/experience/index.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { experiences } from '@/assets/datas/experiences';
+import ExperienceList from '@/components/experience-list';
+import Watch from '@/assets/svg/watch.svg';
+import WatchDark from '@/assets/svg/watch-dark.svg';
+import WatchMbDark from '@/assets/svg/watch-mb-dark.svg';
+import useDevice from '@/hooks/use-device';
+
+type ContactElementProps = {
+  theme: string;
+};
+
+const ExperienceElement = ({ theme }: ContactElementProps) => {
+  const { isMobile } = useDevice();
+
+  const watchImage = theme === 'light' ? Watch : WatchDark;
+
+  return (
+    <div className="relative w-full h-screen flex flex-col space-y-2 md:space-y-6 py-14">
+      <h1 className="text-center font-montserrat text-5xl font-semibold px-4 md:px-16 lg:px-32">
+        Experiences
+      </h1>
+      <div className="flex items-center flex-1 gap-2 md:gap-4 lg:gap-6">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8 }}
+          className="relative w-2/5 aspect-square hidden lg:block pl-4 md:pl-16 lg:pl-32"
+        >
+          <Image
+            src={watchImage}
+            alt="watch"
+            className="w-full h-full p-10 object-contain inset-0"
+          />
+        </motion.div>
+        <div className="flex-1 relative">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8 }}
+            className="absolute right-0 w-2/3 block lg:hidden -top-5"
+          >
+            <Image
+              src={WatchMbDark}
+              alt="watch"
+              className="w-full h-full object-cover inset-0"
+            />
+          </motion.div>
+          <Accordion
+            type="single"
+            collapsible
+            className="w-full flex flex-col justify-start gap-4 h-full pl-4 md:pl-0 pr-4 md:pr-16 lg:pr-32"
+            defaultValue="0"
+          >
+            {experiences.map((item, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: index * 0.5 }}
+                viewport={{ once: true }}
+                className="border-b border-tertiary/50 dark:border-tertiary/10 px-4"
+              >
+                <AccordionItem value={`${item.id}`}>
+                  <AccordionTrigger className="w-full cursor-pointer pb-2 hover:no-underline hover:scale-[1.02]">
+                    <ExperienceList
+                      key={index}
+                      periode={item.periode}
+                      role={item.role}
+                      office={item.office}
+                    />
+                  </AccordionTrigger>
+                  <AccordionContent className="flex flex-col gap-4 text-balance py-2 w-full">
+                    <div className="w-full text-sm">{item.description}</div>
+                  </AccordionContent>
+                </AccordionItem>
+              </motion.div>
+            ))}
+          </Accordion>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExperienceElement;
